63baa91c62fdefcc45d97ffdbca3eb2ef2857598
Caioy <caio.yugo11@gmail.com>
2024-10-17 21:03:45 -0300

    feat: atv n2-3

---
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-3/quicksort.c
@@ -0,0 +1,71 @@
+/*--------------------------------------------------------*
+* Disciplina: Programaçao Estruturada e Modular           *
+*          Prof. Carlos Veríssimo                         *
+*-------------------------------------------------------- *
+* Objetivo do Programa: QuickSort                         *
+* Data - 17/10/2024                                       * 
+* Autores: Caio Yugo Saeda Uemura                         *
+*--------------------------------------------------------*/
+
+#include <stdio.h>
+
+// Função que realiza a troca entre dois elementos
+void troca(int arr[], int i, int j) {
+    int aux = arr[i];
+    arr[i] = arr[j];
+    arr[j] = aux;
+}
+
+// Função que realiza a partição e retorna o índice do pivô
+int particao(int arr[], int limInf, int limSup) {
+    int pivo = arr[limSup]; // O pivô é o último elemento
+    int pivo_indice = limInf; // Índice do pivô inicial
+
+    for (int i = limInf; i < limSup; i++) {
+        // Verifica se o elemento é <= ao pivô
+        if (arr[i] <= pivo) {
+            // Realiza a troca
+            troca(arr, i, pivo_indice);
+            // Incrementa o pivo_indice
+            pivo_indice++;
+        }
+    }
+
+    // Troca o pivô
+    troca(arr, pivo_indice, limSup);
+
+    // Retorna o índice do pivô
+    return pivo_indice;
+}
+
+// Função que implementa o algoritmo QuickSort
+void quickSort(int arr[], int limInf, int limSup) {
+    if (limInf < limSup) {
+        // Realiza a partição e obtém o índice do pivô
+        int pivo_indice = particao(arr, limInf, limSup);
+
+        // Ordena os elementos antes e depois da partição
+        quickSort(arr, limInf, pivo_indice - 1);
+        quickSort(arr, pivo_indice + 1, limSup);
+    }
+}
+
+// Função para imprimir o array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {100, 70, 90, 90, 10, 40, 30};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    printf("Array original:\n");
+    printArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    printf("Array ordenado:\n");
+    printArray(arr, n);
+
+    return 0;
+}
